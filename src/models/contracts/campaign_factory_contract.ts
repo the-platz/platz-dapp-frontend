bb497d47636014c55ec70a478350a3953f4456d6
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-02-09 11:59:11 +0000

    feat: cp factory options

--- src/models/contracts/campaign_factory_contract.ts
+++ src/models/contracts/campaign_factory_contract.ts
@@ -1,9 +1,15 @@
 import { Contract } from "near-api-js"
+import { ContractMethods } from "near-api-js/lib/contract"
 
 export type CampaignFactoryInfo = {
     account_campaigns: string[]
     contract_owner: string
     punkt_contract_account_id: string
+}
+
+const CampaignContractOptions: ContractMethods = {
+    viewMethods: ['get_campaign_factory_info'],
+    changeMethods: ['create_campaign']
 }
 
 export type CampaignContractFactory = Contract & {
