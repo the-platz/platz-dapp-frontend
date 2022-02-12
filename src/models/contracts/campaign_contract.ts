22e24c1ec43e82ef9ae3c1e9ca3a73f9b935bbcc
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-02-12 10:25:06 +0000

    fix: use app url in tx call back url

--- src/models/contracts/campaign_contract.ts
+++ src/models/contracts/campaign_contract.ts
@@ -2,6 +2,7 @@ import BN from 'bn.js';
 import { Contract, WalletConnection } from "near-api-js"
 import { ContractMethods } from "near-api-js/lib/contract"
 import { IChangeMethodFn, ChangeMethodOptions } from "./interfaces"
+import * as env from "../../env"
 
 type CampaignInfo = {
     campaign_beneficiary: string
@@ -48,7 +49,7 @@ export const donateAsync = async(campaignContract: CampaignContract, donationAmo
     if (campaignContract.donate) {
         const changeMethodOptions: ChangeMethodOptions = {
             meta: `You made the transaction to donate the campaign "${campaignContract.contractId}".`,
-            callbackUrl: `http://localhost:3000/txCallback/campaign/${campaignContract.contractId}/donate`,
+            callbackUrl: `${env.APP_URL}/txCallback/campaign/${campaignContract.contractId}/donate`,
             args: {},
             amount: donationAmount,
             // gas auto fill
@@ -63,7 +64,7 @@ export const withdrawAsync = async(campaignContract: CampaignContract) => {
     if (campaignContract.withdraw) {
         const changeMethodOptions: ChangeMethodOptions = {
             meta: `You made the transaction to claim the campaign "${campaignContract.contractId}".`,
-            callbackUrl: `http://localhost:3000/txCallback/campaign/${campaignContract.contractId}/withdraw`,
+            callbackUrl: `${env.APP_URL}/txCallback/campaign/${campaignContract.contractId}/withdraw`,
             args: {},
             // no deposit
             // gas auto fill
